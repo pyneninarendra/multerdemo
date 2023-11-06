9252e22872847f4dc13144b40849119acf99771b
pyneninarendra@gmail.com <pyneninarendra@gmail.com>
2023-11-06 22:12:43 +0530

    Multer: Upload images filter type

--- server.js
+++ server.js
@@ -18,13 +18,14 @@ var multerStorage = multer.diskStorage({
         cb(null, 'images')
     },
     filename: (req, file, cb) => {
-        console.log(file)
-        cb(null, Date.now() + "-"+ file.originalname + "" + path.extname(file.originalname))
+        cb(null, Date.now() + "-" + file.originalname + "" + path.extname(file.originalname))
     }
 })
 
 var multerFilter = (req, file, cb) => {
     if (file.mimetype.split("/")[1] === "png") {
+        cb(null, true)
+    } else if (file.mimetype.split("/")[1] === "jpg") {
         cb(null, true)
     } else {
         cb(new Error("Not supported file"), false)
